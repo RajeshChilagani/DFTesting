6978d4786e7e6f3737ae871ddb213ef51a1e8427
Rajesh <chilaganirajesh95@gmail.com>
2020-05-05 22:41:19 -0600

    Initial Commit

---
+++ DF/Assets/Dragon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Material MyMaterial;
+    int emote = 0;
+    Color[] Emotes = {Color.green,Color.blue,Color.red};
+    void Start()
+    {
+        MyMaterial = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(emote);
+        switch (emote)
+        {
+            case 0: MyMaterial.color = Color.Lerp(MyMaterial.color, Emotes[emote], 1 * Time.deltaTime);
+                break;
+            case 1:
+                MyMaterial.color = Color.Lerp(MyMaterial.color, Emotes[emote], 1 * Time.deltaTime);
+                break;
+            case 2:
+                MyMaterial.color = Color.Lerp(MyMaterial.color, Emotes[emote], 1 * Time.deltaTime);
+                break;
+
+        }
+        
+    }    
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if(other.tag.Equals("Hand"))
+        {
+            Debug.Log(other.name);
+            Debug.Log(other.GetComponentInParent<HandMovement>().YRotationSpeed);
+            
+            if (other.GetComponentInParent<HandMovement>().YRotationSpeed>5)
+            {
+                emote = 2;
+            }
+            else if(other.GetComponentInParent<HandMovement>().YRotationSpeed > 2)
+            {
+                emote = 1;
+            }
+            else
+            {
+                emote = 0;
+            }
+        }
+    }
+}
---
+++ DF/Assets/HandMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Left;
+    public GameObject Right;
+    public float Speed = 10;
+    public  float YRotationSpeed;
+    public float ZRotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q))
+        {
+            YRotationSpeed += Time.deltaTime;
+            //Rotation = Mathf.Clamp(Rotation, -90f, 90f);
+            Left.transform.RotateAround(transform.position,transform.up, YRotationSpeed);
+            Right.transform.RotateAround(transform.position, transform.up, -YRotationSpeed);
+        }
+        //else
+        //{
+        //    if (YRotationSpeed > 0)
+        //    {
+        //        YRotationSpeed = 0;
+        //    }
+        //}
+        else if (Input.GetKey(KeyCode.E))
+        {
+            YRotationSpeed += Time.deltaTime;
+            //Rotation = Mathf.Clamp(Rotation, -90f, 90f);
+            Left.transform.RotateAround(transform.position, transform.up, -YRotationSpeed);
+            Right.transform.RotateAround(transform.position, transform.up, YRotationSpeed);
+        }
+        else
+        {
+            YRotationSpeed = 0;
+        }
+
+    }
+}
