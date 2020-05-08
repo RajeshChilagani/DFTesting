4fc8308023856ff076f62ab39002571c18d87cea
Rajesh <chilaganirajesh95@gmail.com>
2020-05-07 19:25:46 -0600

    Updates

--- DF/Assets/HandMovement.cs
+++ DF/Assets/HandMovement.cs
@@ -102,6 +102,10 @@ public class HandMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
         if(Input.GetKey(KeyCode.Alpha1))
         {
             Left.transform.localPosition = new Vector3(-0.2f, -0.5f, 0f);
